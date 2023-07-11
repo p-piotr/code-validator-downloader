8b385d83d270ee219bcb1648db68d87014485227
PIZDINIO <piotr.pazdan.382@gmail.com>
2023-07-11 14:11:28 +0200

    v0.1.0

--- download-page-template.php
+++ download-page-template.php
@@ -24,13 +24,13 @@ function add_download_log($dynamic_path, $serial_code, $product_id)
     //echo $wpdb->last_error;
 }
 
-function send_file($file_path)
+function send_file($file_path, $file_ext_name)
 {
     $finfo = finfo_open(FILEINFO_MIME_TYPE);
     header('Content-Type: ' . finfo_file($finfo, $file_path));
     finfo_close($finfo);
     //Use Content-Disposition: attachment to specify the filename
-    header('Content-Disposition: attachment; filename='.basename($file_path));
+    header('Content-Disposition: attachment; filename='.$file_ext_name);
     //No cache
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
@@ -49,7 +49,6 @@ function check_path($dynamic_path)
     require_once('serial-code-status.php');
     global $wpdb, $table_name_products, $table_name_dynamic_links, $table_name_downloads, $product_downloads_amount, 
         $shortcode_rendered_url;
-
         
     $sql = "SELECT * FROM $table_name_dynamic_links INNER JOIN $table_name_products
         ON $table_name_dynamic_links.product_id = $table_name_products.product_id
@@ -58,6 +57,7 @@ function check_path($dynamic_path)
     $file_path = $result[0]->file_url;
     $serial_code = $result[0]->serial_code;
     $product_id = $result[0]->product_id;
+    $file_ext_name = $result[0]->file_ext_name;
     if (is_serial_code_valid($serial_code)['status'] == CODE_RESULT_EXPIRED)
     {
         redirect($_SESSION['shortcode_rendered_url'] . "?serial_code=" . $serial_code);
@@ -76,7 +76,7 @@ function check_path($dynamic_path)
     }
 
     // plik istnieje oraz kod nadal jest ważny, można przesłać plik
-    return array('serial_code' => $serial_code, 'product_id' => $product_id, 'file_path' => $file_path);
+    return array('serial_code' => $serial_code, 'product_id' => $product_id, 'file_path' => $file_path, 'file_ext_name' => $file_ext_name);
 }
 
 $path = get_query_var('download'); // dynamiczna sciezka dostepu
@@ -86,9 +86,10 @@ if ($check_result != null)
     $serial_code = $check_result['serial_code'];
     $product_id = $check_result['product_id'];
     $file_path = $check_result['file_path'];
+    $file_ext_name = $check_result['file_ext_name'];
     add_download_log($dynamic_path, $serial_code, $product_id);
     //echo $file_path . ' sent';
-    send_file($file_path);
+    send_file($file_path, $file_ext_name);
 }
 else
     echo 'empty';
