9e94cb72cb992b83f75c7711883ca407f3642566
piotr <piotr.pazdan.200@gmail.com>
2023-10-29 15:44:12 +0100

    0.5.0

--- js/client-dialog-handler.js
+++ js/client-dialog-handler.js
@@ -1,18 +1,18 @@
 function show() {
-  let dialog = document.getElementById("dialog");
-  function showDialog() {
-    dialog.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") {
-        e.preventDefault();
-      }
-    });
-    dialog.showModal();
-  }
-  showDialog();
+	let dialog = document.getElementById("dialog");
+	function showDialog() {
+		dialog.addEventListener("keydown", (e) => {
+			if (e.key === "Escape") {
+				e.preventDefault();
+			}
+		});
+		dialog.showModal();
+	}
+	showDialog();
 }
 
 function performClose() {
-  document.getElementById("dialog").close();
+	document.getElementById("dialog").close();
 }
 /*
 window.onload = (event) => {
@@ -20,11 +20,10 @@ window.onload = (event) => {
 };
 */
 
-window.addEventListener ? window.addEventListener("load", show, false) : 
-  window.attachEvent && window.attachEvent("onload", show);
+window.addEventListener ? window.addEventListener("load", show, false) :
+	window.attachEvent && window.attachEvent("onload", show);
 
-function redirect(newUrl)
-{
-  window.open(newUrl, "_self");
-  document.getElementById("div_forbidden").remove();
+function redirect(newUrl) {
+	window.open(newUrl, "_self");
+	document.getElementById("div_forbidden").remove();
 }
